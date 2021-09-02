c7e4fd3f8053e6dc226fee8b684b72ae906f3173
rizqiro <rizqi-romadhon@outlook.com>
2021-09-02 22:35:07 +0700

    third commit

--- lib/worksandeducation.dart
+++ lib/worksandeducation.dart
@@ -93,7 +93,68 @@ class _WorksAndEducationState extends State<WorksAndEducation> {
                 )),
           );
         } else {
-          return Container();
+          return Container(
+            width: size.width,
+            child: Container(
+                color: Colors.white,
+                alignment: Alignment.center,
+                child: Container(
+                  width: size.width * 0.7,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SelectableText('Qualifications',
+                          style: GoogleFonts.roboto(
+                            color: orangeColor,
+                            fontSize:20,)),
+                      SelectableText('My Awesome Journey',
+                          style: GoogleFonts.notoSans(
+                              color: darkBlueColor,
+                              fontSize:40,
+                              fontWeight: FontWeight.w700)),
+                      SizedBox(height: 30,),
+                      Container(
+                        width: size.width,
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset('assets/images/education.png', color: darkBlueColor, width: 30, height: 30 * (79/115),),
+                                SizedBox(width: 10,),
+                                Text('Education', style: GoogleFonts.lato(
+                                    color: darkBlueColor,
+                                    fontSize:22,
+                                    fontWeight: FontWeight.w700))
+                              ],
+                            ),
+                            SizedBox(height: 30,),
+                            WorksEduListTile(title: 'Politeknik TEDC Bandung', subtitle: 'Mechatronics', year: '2012 - 2016'),
+                            WorksEduListTile(title: 'BBPLK Bandung', subtitle: 'Mechanical CAD Drafter', year: '2014 - 2014'),
+                            WorksEduListTile(title: 'SMK Negeri 5 Banjarmasin', subtitle: 'Automotive', year: '2009 - 2012'),
+                            WorksEduListTile(title: 'LBPP LIA Banjarmasin', subtitle: 'Advanced English', year: '2010 - 2011'),
+                            SizedBox(height: 30,),
+                            Row(
+                              children: [
+                                Image.asset('assets/images/work.png', color: darkBlueColor, width: 30, height: 30 * (79/115),),
+                                SizedBox(width: 10,),
+                                Text('Experience', style: GoogleFonts.lato(
+                                    color: darkBlueColor,
+                                    fontSize:22,
+                                    fontWeight: FontWeight.w700))
+                              ],
+                            ),
+                            SizedBox(height: 30,),
+                            WorksEduListTile(title: 'Asahimas Flat Glass, Tbk', subtitle: 'Production Engineer', year: '2016 - Now'),
+                            WorksEduListTile(title: 'United Tractor Pandu Engineering', subtitle: 'Engineer\'s assistant', year: '2015 - 2016'),
+                            WorksEduListTile(title: 'Tekno Multi Utama', subtitle: 'Microcontroller Programmer', year: '2015 - 2015'),
+                            WorksEduListTile(title: 'Transifex', subtitle: 'Translate Contributor', year: '2012 - 2014'),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+          );
         }
       },
     );
